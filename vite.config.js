bc357706d3c1d1dc1aa0a24db72d23cdc03cd416
server_merchus <merchussoft@hotmail.com>
2024-06-27 00:43:21 -0500

    se ajusta para que heroku lo detecte y lo despliegue bien

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
+  server: {
+    headers: {
+      // Deshabilitar 'interest-cohort' en el encabezado Permissions-Policy
+      'Permissions-Policy': 'interest-cohort=()'
+    }
+  }
 })
