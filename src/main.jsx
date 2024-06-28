c19cdd8da9f8265c8107dbe3bc4a48b8ecacdccd
server_merchus <merchussoft@hotmail.com>
2024-06-28 01:20:18 -0500

    se hace un nuevo despliegue revisando que se pueda mantener lo de las rutas

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
